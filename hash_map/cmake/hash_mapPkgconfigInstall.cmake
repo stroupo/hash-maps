1b2d285c48eb283272c3a250927389cd2588caf5
lyrahgames <markuspawellek@gmail.com>
2018-11-26 14:15:15 +0100

    Restructure CMake scripts

---
+++ hash_map/cmake/hash_mapPkgconfigInstall.cmake
@@ -0,0 +1,11 @@
+configure_file(
+  cmake/${PROJECT_NAME}.pc.in
+  ${PROJECT_NAME}.pc
+  @ONLY
+)
+install(
+  FILES
+    ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.pc
+  DESTINATION
+    ${pkgconfig_install_dir}
+)
